7d819d207ca4e3351df2bbb8f6c5c9ac5bf21a43
tmiomr <118784088+tmiomr@users.noreply.github.com>
2023-06-29 14:49:17 +0900

    Add files via upload

--- stapp_img.py
+++ stapp_img.py
@@ -95,7 +95,7 @@ def teachable_machine_classification(img, weights_file):
 def main():
 
     # タイトルの表示
-    st.title("城判別")
+    st.title("Image Classification with Google's Teachable Machine")
 
     # アップローダの作成
     uploaded_file = st.file_uploader("Choose a Image...", type="jpg")
@@ -122,11 +122,9 @@ def main():
 
         # 推論で得られたクラス番号(初期値は0)によって出力結果を分岐
         if classNo == 0:
-            st.subheader(f"これは{pred0}％の確率で「siro1」です！")
+            st.subheader(f"これは{pred0}％の確率で「猫」です！")
         else:
-            st.subheader(f"これは{pred1}％の確率で「siro2」です！")
-
-
+            st.subheader(f"これは{pred1}％の確率で「犬」です！")
 
 
 # mainの起動
